870ab2090a31c980dc8982cf841d194e99a93060
Jon Lebensold <jon@lebensold.ca>
2012-04-29 21:30:27 +0300

    setup CRUD for categories + view code for adding

--- app.rb
+++ app.rb
@@ -1,9 +1,64 @@
 set :public_folder, File.dirname(__FILE__) + '/public'
 
 
-get '/q/:term' do
+# FIND ALL
+get '/categories' do
   content_type :json
-  out.to_json()
+  Category.find(:all).to_json()
+end
+
+get '/shoppinglists' do
+  content_type :json
+  ShoppingList.find(:all).to_json()
+end
+
+get '/shopitems' do
+  content_type :json
+  ShopItem.find(:all).to_json()
+end
+
+# CREATE
+post '/categories' do 
+  content_type :json
+  Category.new(JSON.parse(request.body.read)).save().to_json()
+end
+
+post '/shopitems' do 
+  content_type :json
+  ShopItem.new(JSON.parse(request.body.read)).save().to_json()
+end
+
+post '/shoppinglists' do 
+  content_type :json
+  ShoppingList.new(JSON.parse(request.body.read)).save().to_json()
+end
+
+# DELETE
+delete '/:type/:id' do
+  content_type :json
+  case params[:type]
+  when "shopitem"
+      return ShopItem.find(params[:id]).destroy().to_json()
+  when "category"
+      return Category.find(params[:id]).destroy().to_json()
+  when "shoppinglist"
+      return ShoppingList.find(params[:id]).destroy().to_json()
+  end
+  return {}.to_json()
+end
+
+# FIND ONE 
+get '/:type/:id' do
+  content_type :json
+  case params[:type]
+  when "shopitem"
+      return ShopItem.find(params[:id]).to_json()
+  when "category"
+      return Category.find(params[:id]).to_json()
+  when "shoppinglist"
+      return ShoppingList.find(params[:id]).to_json()
+  end
+  return {}.to_json()
 end
 
 
