5cbff9b335da0a590cbb577eaeae06b922f78351
Jon Lebensold <jon@lebensold.ca>
2012-05-05 16:39:00 +0300

    touch ups

--- app.rb
+++ app.rb
@@ -69,26 +69,6 @@ end
 
 
 get '/' do
-#  Category.delete_all()
-#  ShopItem.delete_all()
-#  ShoppingList.delete_all()
-
-
-#  c = Category.new({name: "Home Hardware"})
-#  c.save()
-
-#  si = ShopItem.new({name:"carrots", category: c})
-#  si.save()
-
-
-#  si2 = ShopItem.new({name:"turnips", category:c})
-#  si2.save()
-
-#  sl = ShoppingList.new({shopping_date: Date.parse("January 1, 2000") })
-#  sl.shop_items = [si,si2]
-#  sl.save()
-
-
 	erb :index
 end
 
